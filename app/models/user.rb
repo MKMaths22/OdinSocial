919c466d6eeca16ef97713cd8fbf58312dc92647
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-03-28 15:47:39 +0000

    Put name validation in User model. Generate Devise controller to add :name to permitted params and change routes file to use the generated controller. Generate Devise views and add name fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :rememberable, :validatable
+  validates :name, presence: true
   # has_one :profile
   has_many :authored_posts, class_name: "Post", foreign_key: "author_id" 
 end
