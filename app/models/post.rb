a9a42007e5527aab0c47d8548231a42aab39f8de
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-04-05 16:20:05 +0100

    Add foreign_key to Post model to let Post.first.likes and Post.first.likers commands work.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   # Add a SCOPE to automatically order by date
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many :comments
-  has_many :likes
+  has_many :likes, foreign_key: "liked_post_id"
   has_many :likers, through: :likes
 end
