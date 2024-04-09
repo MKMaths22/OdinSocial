6ba43689c713de751f0d0398f1e656bdc9790629
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-04-09 10:43:25 +0100

    Move authenticate_user and before_action into Application Controller with exception for devise/sessions to DRY controller code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    unless params[:controller] == 'devise/sessions'
+      redirect_to new_user_session_path unless current_user
+    end
+  end
 end
