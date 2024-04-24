85c14d57df24a3b7ccee1f4153606edb54c0d59d
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-04-24 15:41:12 +0100

    Add uniqueness validation to Like model.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,6 @@
 class Like < ApplicationRecord
   default_scope { order(created_at: :desc) }
+  validates_uniqueness_of :liked_post, :scope => :liker
   belongs_to :liker, class_name: "User", foreign_key: "liker_id"
   belongs_to :liked_post, class_name: "Post", foreign_key: "liked_post_id"
 end
