d7c191720f68c1bec9273d0de4f1606eab0ab9bf
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-05-27 13:41:05 +0100

    Add "Signed in as current_user.name" to application layout navbar. Complete the allowed_profile_params in ProfilesController.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,6 +11,6 @@ class ProfilesController < ApplicationController
   private
 
   def allowed_profile_params
-    params.require(:profile).permit(:picture)
+    params.require(:profile).permit(:picture, :body, :field_name_one, :field_content_one, :field_name_two, :field_content_two, :field_name_three, :field_content_three)
   end
 end
