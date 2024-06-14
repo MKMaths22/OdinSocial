7b6a2fe9a47ce5485f13d0d22ae93c9fcfddc5e9
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-06-14 13:42:26 +0100

    Write model tests for User to test that name, email and password are all required to save a new User. Add default_url_options command to test.rb to allow User to save when name, email and password provided.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should not save user without email" do
+    user = User.new(name: 'Peter', password: 'somepassword')
+    assert_not user.save
+  end
+
+  test "should not save user without name" do
+    user = User.new(email: 'peter@gmail.com', password: 'somepassword')
+    assert_not user.save
+  end
+
+  test "should not save user without password" do
+    user = User.new(email: 'peter@gmail.com', name: 'Peter')
+    assert_not user.save
+  end
+
+  test "should save user with name, email and password" do
+    user = User.new(name: 'Somename', email: 'somename@gmail.com', password: 'somepassword')
+    assert user.save
+  end
 end
