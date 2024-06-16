7084cd7ab3cbb7050f64f7992b1a9ce8d82f7380
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-06-16 10:03:06 +0100

    Write tests in follow_test.rb and follow_request_test.rb checking uniqueness validations, using fixtures in users.yml, follows.yml and follow_requests.yml.

--- test/models/follow_request_test.rb
+++ test/models/follow_request_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class FollowRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a duplicate follow request" do
+    request = FollowRequest.new(requester: FollowRequest.first.requester, requestee: FollowRequest.first.requestee)
+    assert_not request.save, "Saved a duplicated follow request"
+  end
+
+  test "should save a follow request with duplicate requester but different requestee" do
+    # test depends on fixtures follow_requests.yml and users.yml, that only existing follow request is chris requesting to follow peter, with ste as another user
+    request = FollowRequest.new(requester: User.find_by(name: 'Chris'), requestee: User.find_by(name: 'Ste'))
+    assert request.save, "Failed to save a follow request with duplicated requester but different requestee"
+  end
+
+  test "should save a follow request with duplicated requestee but different requester" do
+    # test depends on fixtures follow_requests.yml and users.yml, that only existing follow request is chris requesting to follow peter, with ste as another user
+    request = FollowRequest.new(requester: User.find_by(name: 'Ste'), requestee: User.find_by(name: 'Peter'))
+    assert request.save, "Failed to save a follow request with duplicated requestee but different requester"
+  end
 end
--- test/models/follow_test.rb
+++ test/models/follow_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class FollowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should not save a duplicate follow" do
+    follow = Follow.new(follower: Follow.first.follower, followee: Follow.first.followee)
+    assert_not follow.save, "Saved a duplicated follow"
+  end
+
+  test "should save a follow with duplicate follower but different followee" do
+    # test depends on fixtures follows.yml and users.yml, that only existing follow is peter following chris, with ste as another user
+    follow = Follow.new(follower: User.find_by(name: 'Peter'), followee: User.find_by(name: 'Ste'))
+    assert follow.save, "Failed to save a follow with duplicated follower but different followee"
+  end
+
+  test "should save a follow with duplicated followee but different follower" do
+    # test depends on fixtures follows.yml and users.yml, that only existing follow is peter following chris, with ste as another user
+    follow = Follow.new(follower: User.find_by(name: 'Ste'), followee: User.find_by(name: 'Chris'))
+    assert follow.save, "Failed to save a follow with duplicated followee but different follower"
+  end
 end
