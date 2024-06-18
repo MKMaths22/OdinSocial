fafe5f9b2e9424cacb9e2152e555a0d19b4ff1a5
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-06-18 11:23:41 +0100

    Bug caused when trying to add Devise helpers to use sign_in method for testing the PostsController#index

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::ControllerHelpers
+  
+  test "should not get index if not signed in" do
+    get posts_url 
+    assert_response :redirect, "found posts url despite not signed in"
+  end
+
+  test "signed in user should get index page" do
+    sign_in User.first
+    get :index
+    assert_response :success
+  end
 end
