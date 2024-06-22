343773ec617024c3048b6e2a3af74c4214d3e46c
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-06-22 11:02:19 +0100

    Add update tests to posts_controller_test to check that signed_in users can update posts but only if they authored the posts.

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -12,4 +12,22 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     get posts_url
     assert_response :success, "signed in user did not reach posts#index view"
   end
+
+  test "signed in user should not be able to update post they did not author" do
+    sign_in users(:peter)
+    @post = Post.find_by(author: users(:chris))
+    post_params = { post: {title: 'NewTitle'} }
+    patch post_url(@post), params: post_params
+    @post.reload
+    assert_not_equal('NewTitle', @post.title)
+  end
+
+  test "signed in user should be able to update post they authored" do
+    sign_in users(:peter)
+    @post = Post.find_by(author: users(:peter))
+    post_params = { post: {title: 'NewTitle'} }
+    patch post_url(@post), params: post_params
+    @post.reload
+    assert_equal('NewTitle', @post.title)
+  end
 end
