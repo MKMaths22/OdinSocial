4623f1c387bff1fa053405ddb79f8eedfaae766e
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-08-03 16:21:20 +0100

    Complete system tests for follow_requests for Declining, Accepting, Withdrawing and Creating requests.

--- test/system/follow_requests_test.rb
+++ test/system/follow_requests_test.rb
@@ -13,19 +13,42 @@ class FollowRequestsTest < ApplicationSystemTestCase
     sign_in @peter
     visit 'users/registrations'
     click_on "Decline"
+    assert_text "You have declined the follow request from Chris."
+    assert_text "You have no incoming Follow requests."
+    sign_in @chris
+    visit 'users/registrations'
+    assert_text "You are not currently following anyone."
   end
 
   test "Accepting a follow request" do
-
+    sign_in @peter
+    visit 'users/registrations'
+    click_on "Accept"
+    assert_text "You are now being followed by Chris."
+    sign_in @chris
+    visit 'users/registrations'
+    assert_text 'Unfollow'
   end
 
   test "Withdrawing a follow request" do
-
+    sign_in @chris
+    visit 'users/registrations'
+    click_on 'Cancel Request'
+    assert_text 'You have withdrawn your request to follow Peter.'
+    sign_in @peter
+    visit 'users/registrations'
+    assert_text 'You have no incoming Follow requests.'
   end
 
   test "Creating a follow request" do
-
+    sign_in @ste
+    visit 'users/registrations'
+    assert_text 'You currently have no outgoing Follow requests.'
+    click_on 'Request to follow Chris'
+    assert_text 'You have made a request to follow Chris.'
+    sign_in @chris
+    visit 'users/registrations'
+    assert_text 'Decline'
+    assert_text 'You have no incoming Follow requests.', count: 0
   end
-
-
 end
