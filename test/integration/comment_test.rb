2fb49d496df94342d702705c576b87d9b2fc4ca8
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-08-09 16:17:12 +0100

    Have unconfirmed user Reginald commented out in users.yml and in comment_test Integration test use Post.first.author to sign in to ensure their added comment shows after the redirect, otherwise "can create a comment" can fail.

--- test/integration/comment_test.rb
+++ test/integration/comment_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class CommentTest < ActionDispatch::IntegrationTest
   test "can create a comment" do
-    sign_in (User.first)
+    sign_in (Post.first.author)
+    # this is relevant because of the way the comments controller redirects you after adding a comment. If another user added a comment to a post not authored by them or someone they followed, their comment would not show after the redirect.
     post "/comments", params: { comment: { post_id: Post.first.id, body: "This is my new comment." }  }
     assert_response :redirect
     follow_redirect!
@@ -12,14 +13,14 @@ class CommentTest < ActionDispatch::IntegrationTest
   end
 
   test "can edit a comment" do
-    sign_in (User.first)
-    comment_id = Comment.find_by(author: User.first).id
+    sign_in users(:peter)
+    comment_id = Comment.find_by(author: users(:peter)).id
     patch "/comments/#{comment_id}", params: { comment: { body: "The edited comment." } }
     assert_response :redirect
     follow_redirect!
     assert_response :success
-    assert_select "p", "Comment updated successfully."
-    assert_select "p", "The edited comment."
+    assert_select "p.notice", "Comment updated successfully."
+    assert_select "p.commentbody", "The edited comment."
   end
 
 end
