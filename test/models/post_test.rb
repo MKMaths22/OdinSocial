8582c7aaaa18dbd3960f6be332d392050d37697f
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2024-08-09 13:04:29 +0100

    Rename classes for test/models/comment_test.rb and test/models/post_test.rb to solve class name conflicts. Comment out unconfirmed user in users.yml, so still no fixture for an unconfirmed user for testing mailer.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PostTest < ActiveSupport::TestCase
+class Post_Test < ActiveSupport::TestCase
   test "the truth" do
      assert true, "Failed to assert the truth"
   end
